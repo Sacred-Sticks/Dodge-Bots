5c9ca57c7ec1ad3c68ae931001317950bf5b514b
Lucas Ackman <AckmanLD@gmail.com>
2024-02-03 22:49:10 -0600

    Set Locomotion Mechanics with Speed Limit

--- Dodge-Bots/Assets/_Project/_Scrpts/Controller/LocomotionController.cs
+++ Dodge-Bots/Assets/_Project/_Scrpts/Controller/LocomotionController.cs
@@ -11,10 +11,11 @@ namespace Dodge_Bots
         [SerializeField] private float jumpHeight;
 
         protected bool isGrounded;
-        private Vector3 activeVelocity;
+        private Vector3 airborneVelocity;
+        private Vector3 initialAirborneVelocity;
 
         private Coroutine trampolineRoutine;
-        
+
         // Cached References & Constant Values
         protected Rigidbody body;
         private float jumpVelocity;
@@ -23,7 +24,7 @@ namespace Dodge_Bots
         private const float radiusMultiplier = 0.5f;
         private const float groundDistance = 1f;
         private float groundRadius;
-        
+
         #region UnityEvents
         private void Awake()
         {
@@ -37,24 +38,56 @@ namespace Dodge_Bots
             groundRadius = capsule.radius * radiusMultiplier;
         }
         #endregion
-        
+
+        // protected void MoveTowards(Vector3 direction)
+        // {
+        //     direction = (transform.right * direction.x + transform.forward * direction.z).normalized;
+        //     if (direction == Vector3.zero && isGrounded)
+        //         DecelerateLocalVelocity(); // only decelerate local velocity when grounded to mimic friction
+        //     var currentVelocity = Vector3.ProjectOnPlane(body.velocity, transform.up);
+        //     float directionAccuracy = Vector3.Dot(direction, currentVelocity) - 1;
+        //     if (airborneVelocity.sqrMagnitude >= movementSpeed * movementSpeed && !isGrounded)
+        //         if (directionAccuracy < tolerance)
+        //             return; // prevent overcorrecting velocity mid-air
+        //     if (directionAccuracy > tolerance && currentVelocity.sqrMagnitude > movementSpeed * movementSpeed)
+        //         return; // prevent moving too fast
+        //     var acceleration = direction * (movementSpeed * accelerationRate);
+        //     airborneVelocity += acceleration;
+        //     body.AddForce(acceleration, ForceMode.VelocityChange);
+        // }
+
         protected void MoveTowards(Vector3 direction)
         {
-            direction = (transform.right * direction.x + transform.forward * direction.z).normalized;
-            if (direction == Vector3.zero && isGrounded)
-                DecelerateLocalVelocity(); // only decelerate local velocity when grounded to mimic friction
+            direction = direction.x * transform.right + direction.z * transform.forward;
+            if (!isGrounded)
+            {
+                AirborneMoveTowards(direction);
+                return;
+            }
             var currentVelocity = Vector3.ProjectOnPlane(body.velocity, transform.up);
-            float directionAccuracy = Vector3.Dot(direction, currentVelocity) - 1;
-            if (activeVelocity.sqrMagnitude >= movementSpeed * movementSpeed && !isGrounded)
-                if (directionAccuracy < tolerance)
-                    return; // prevent overcorrecting velocity mid-air
-            if (directionAccuracy > tolerance && currentVelocity.sqrMagnitude > movementSpeed * movementSpeed)
-                return; // prevent moving too fast
-            var acceleration = direction * (movementSpeed * accelerationRate);
-            activeVelocity += acceleration;
-            body.AddForce(acceleration, ForceMode.VelocityChange);
+            var desiredVelocity = direction * movementSpeed;
+            var deltaVelocity = (desiredVelocity - currentVelocity) * accelerationRate;
+            body.AddForce(desiredVelocity - currentVelocity, ForceMode.VelocityChange);
         }
-        
+
+        private void AirborneMoveTowards(Vector3 direction)
+        {
+            // Modify to allow for movement backwards or sideways, but forwards is fully clamped
+            if (direction == Vector3.zero)
+                return;
+            // if ((airborneVelocity + initialAirborneVelocity).sqrMagnitude > movementSpeed * movementSpeed
+            //     && Vector3.Dot(direction, initialAirborneVelocity) < 0)
+            //     return;
+            if (Vector3.Dot(direction, initialAirborneVelocity) > 0)
+                return;
+            if (airborneVelocity.sqrMagnitude > movementSpeed * movementSpeed)
+                return;
+            var desiredVelocity = direction * movementSpeed;
+            var deltaVelocity = (desiredVelocity - airborneVelocity) * accelerationRate;
+            airborneVelocity += deltaVelocity;
+            body.AddForce(deltaVelocity, ForceMode.VelocityChange);
+        }
+
         protected void Jump()
         {
             if (!isGrounded)
@@ -68,15 +101,15 @@ namespace Dodge_Bots
 
         protected void CheckGrounded()
         {
-            var ray = new Ray(transform.position + transform.up, -transform.up);
+            var ray = new Ray(transform.position + Vector3.up, -Vector3.up);
+            bool previouslyGrounded = isGrounded;
             isGrounded = Physics.SphereCast(ray, groundRadius, groundDistance);
+            if (!previouslyGrounded || isGrounded)
+                return;
+            initialAirborneVelocity = Vector3.ProjectOnPlane(body.velocity, Vector3.up);
+            airborneVelocity = Vector3.zero;
         }
 
-        private void DecelerateLocalVelocity()
-        {
-            activeVelocity -= activeVelocity * (movementSpeed * accelerationRate);
-        }
-        
         #region Notifications
         public enum Event
         {
