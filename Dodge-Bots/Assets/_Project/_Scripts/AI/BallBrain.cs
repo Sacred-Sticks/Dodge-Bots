1a6bbeab7cf659f14a8138c368be9d9b2dc758c5
Lucas Ackman <AckmanLD@gmail.com>
2024-03-13 00:52:20 -0500

    Added BallBrain Compoenent

---
+++ Dodge-Bots/Assets/_Project/_Scripts/AI/BallBrain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Dodge_Bots
+{
+    public class BallBrain : Ball, IBallBrain
+    {
+        private Transform _target;
+
+        private const float delayDuration = 0.5f;
+
+        private IEnumerator TargetFollower()
+        {
+            var delay = new WaitForSeconds(delayDuration);
+            while (_target != null)
+            {
+                yield return delay;
+            }
+        }
+
+        #region BallBrain
+        public void FollowTarget(Transform target)
+        {
+            _target = target;
+            StartCoroutine(TargetFollower());
+        }
+
+        public void LoseTarget()
+        {
+            _target = null;
+            StopAllCoroutines();
+        }
+        #endregion
+    }
+
+    public interface IBallBrain
+    {
+        void FollowTarget(Transform target);
+        void LoseTarget();
+    }
+}
